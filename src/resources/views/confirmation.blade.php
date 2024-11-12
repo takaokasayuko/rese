fc12c13b6fb01d8cc806585bc9ba106a9d3aecb0
takaoka sayuko <sp7y3dx9snn@gmail.com>
2024-11-12 23:39:26 +0900

    qrcodeメール送信リンク変更

---
+++ src/resources/views/confirmation.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/confirmation.css') }}">
+@endsection
+<div class="content">
+  <h2 class="reservation_ttl">ご予約情報</h2>
+
+  <table class="table-email">
+    <tr class="email__row">
+      <th class="email__data">予約氏名</th>
+      <td class="email__data">{{ $reservation->reservationUser->name }}&emsp;様</td>
+    </tr>
+    <tr class="email__row">
+      <th class="email__data">店舗名</th>
+      <td class="email__data">{{ $reservation->reservationShop->name }}</td>
+    </tr>
+    <tr class="email__row">
+      <th class="email__data">日時</th>
+      <td class="email__data">{{ \Carbon\Carbon::parse($reservation['date'])->format('Y-m-d H:i') }}</td>
+    </tr>
+    <tr class="email__row">
+      <th class="email__data">人数</th>
+      <td class="email__data">{{ $reservation->person_num }}人</td>
+    </tr>
+  </table>
+</div>
+
+@section('content')
+
+@endsection
--- src/resources/views/emails/qrcode.blade.php
+++ src/resources/views/emails/qrcode.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'ご予約の確認について')
 
 @section('content')
-<p class="message-content">{{ $message_content['user_name'] }}&emsp;様<br>
+<p class="message-content">{{ $reservation['user'] }}&emsp;様<br>
   ご予約いただきありがとうございます。<br>
   ご予約いただいた内容をお知らせします。
 </p>
@@ -11,22 +11,22 @@
 <table class="table">
   <tr class="email__row">
     <th class="email__data">予約者氏名</th>
-    <td class="email__data">{{ $message_content['user_name'] }}&emsp;様</td>
+    <td class="email__data">{{ $reservation['user'] }}&emsp;様</td>
   </tr>
   <tr class="email__row">
     <th class="email__data">店舗名</th>
-    <td class="email__data">{{ $message_content['shop_name'] }}</td>
+    <td class="email__data">{{ $reservation['shop'] }}</td>
   </tr>
   <tr class="email__row">
     <th class="email__data">日時</th>
-    <td class="email__data">{{ $message_content['date'] }}</td>
+    <td class="email__data">{{ $reservation['date'] }}</td>
   </tr>
   <tr class="email__row">
     <th class="email__data">人数</th>
-    <td class="email__data">{{ $message_content['person_num'] }}人</td>
+    <td class="email__data">{{ $reservation['person_num'] }}人</td>
   </tr>
 </table>
 
 <p class="message-footer">当日はお送りしたQRコードを店舗にご提示ください。</p>
-{!! QrCode::encoding('UTF-8')->generate($qrcode) !!}
+{!! QrCode::size(200)->generate($url) !!}
 @endsection
