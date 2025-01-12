5ccbff53c69c83eadb3184f183f3dffee8d2ea6a
takaoka sayuko <sp7y3dx9snn@gmail.com>
2025-01-12 12:04:25 +0900

    詳細・レビューページのレスポンシブデザイン

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -8,97 +8,97 @@
 @parent
 
 <div class="search-box">
-	<form class="search-form" action="/search" method="get">
+  <form class="search-form" action="/search" method="get">
 
-		<div class="search__area">
-			<select class="search__select" name="area">
-				<option selected value="">All area</option>
-				@foreach($areas as $area)
-				<option value="{{ $area }}" @if( request('area')==$area ) selected @endif>{{ $area }}</option>
-				@endforeach
-			</select>
-		</div>
+    <div class="search__area">
+      <select class="search__select" name="area">
+        <option selected value="">All area</option>
+        @foreach($areas as $area)
+        <option value="{{ $area }}" @if( request('area')==$area ) selected @endif>{{ $area }}</option>
+        @endforeach
+      </select>
+    </div>
 
-		<div class="search__genre">
-			<select class="search__select" name="genre">
-				<option selected value="">All genre</option>
-				@foreach($genres as $genre)
-				<option value="{{ $genre['genre'] }}" @if( request('genre')==$genre['genre'] ) selected @endif>{{ $genre['genre'] }}</option>
-				@endforeach
-			</select>
-		</div>
-		<input class="search__button-submit" type="submit" value="">
+    <div class="search__genre">
+      <select class="search__select" name="genre">
+        <option selected value="">All genre</option>
+        @foreach($genres as $genre)
+        <option value="{{ $genre['genre'] }}" @if( request('genre')==$genre['genre'] ) selected @endif>{{ $genre['genre'] }}</option>
+        @endforeach
+      </select>
+    </div>
+    <input class="search__button-submit" type="submit" value="">
 
-		<input class="search__keyword-input" type="text" name="keyword" placeholder="Search …" value="{{ request('keyword') }}">
-	</form>
+    <input class="search__keyword-input" type="text" name="keyword" placeholder="Search …" value="{{ request('keyword') }}">
+  </form>
 </div>
 @endsection
 
 @section('content')
 <div class="content">
 
-	@foreach($shop_favorites as $shop_favorite)
-	<div class="shop__list">
-		<div class="shop__img">
-			@if(str_starts_with($shop_favorite['shop']['image'], 'https://'))
-			<img src="{{ $shop_favorite['shop']['image'] }}" alt="">
-			@else
-			<img src="{{ Storage::url($shop_favorite['shop']['image']) }}" alt="">
-			@endif
+  @foreach($shop_favorites as $shop_favorite)
+  <div class="shop__list">
+    <div class="shop__img">
+      @if(str_starts_with($shop_favorite['shop']['image'], 'https://'))
+      <img src="{{ $shop_favorite['shop']['image'] }}" alt="">
+      @else
+      <img src="{{ Storage::url($shop_favorite['shop']['image']) }}" alt="">
+      @endif
 
-		</div>
-		<div class="shop__container">
-			<div class="shop-tittle__group">
-				<h2 class="shop-name">{{ $shop_favorite['shop']['name'] }}</h2>
+    </div>
+    <div class="shop__container">
+      <div class="shop-tittle__group">
+        <h2 class="shop-name">{{ $shop_favorite['shop']['name'] }}</h2>
 
-				<p class="shop-review">
-					<span class="review-rate" data-rate="{{ $shop_favorite['star'] }}"></span>
-					<span class="review-average">{{ $shop_favorite['average'] }}</span>
-				</p>
-			</div>
+        <p class="shop-review">
+          <span class="review-rate" data-rate="{{ $shop_favorite['star'] }}"></span>
+          <span class="review-average">{{ $shop_favorite['average'] }}</span>
+        </p>
+      </div>
 
-			<div class="shop__container-tag">
-				<p class="shop__container-tag--area">#{{ $shop_favorite['shop']['area'] }}</p>
-				<p class="shop__container-tag--genre">#{{ $shop_favorite['shop']['genre'] }}</p>
-			</div>
-			<div class="shop__container-foot">
-				<div class="shop__container-foot--detail">
-					@auth()
-					<a class="shop-detail__link" href="{{ route('shop.detail', ['shop_id' => $shop_favorite['shop']['id']]) }}">詳しくみる</a>
-					@endauth
-					@guest()
-					<a class="shop-detail__link" href="/login">詳しくみる</a>
-					@endguest()
-				</div>
-				<div class="shop__container-foot--fav">
-					@auth()
-					@if($shop_favorite['favorite'] === 'false')
-					<form class="form__fav" action="/favorite/store" method="post">
-						<input type="hidden" name="shop_id" value="{{ $shop_favorite['shop']['id'] }}">
-						@csrf
-						<button class="button__submit-un_fav">&#10084;</button>
-					</form>
-					@endif
+      <div class="shop__container-tag">
+        <p class="shop__container-tag--area">#{{ $shop_favorite['shop']['area'] }}</p>
+        <p class="shop__container-tag--genre">#{{ $shop_favorite['shop']['genre'] }}</p>
+      </div>
+      <div class="shop__container-foot">
+        <div class="shop__container-foot--detail">
+          @auth()
+          <a class="shop-detail__link" href="{{ route('shop.detail', ['shop_id' => $shop_favorite['shop']['id']]) }}">詳しくみる</a>
+          @endauth
+          @guest()
+          <a class="shop-detail__link" href="/login">詳しくみる</a>
+          @endguest()
+        </div>
+        <div class="shop__container-foot--fav">
+          @auth()
+          @if($shop_favorite['favorite'] === 'false')
+          <form class="form__fav" action="/favorite/store" method="post">
+            <input type="hidden" name="shop_id" value="{{ $shop_favorite['shop']['id'] }}">
+            @csrf
+            <button class="button__submit-un_fav">&#10084;</button>
+          </form>
+          @endif
 
-					@if($shop_favorite['favorite'] === 'true')
-					<form class="form__fav" action="/favorite/delete" method="post">
-						<input type="hidden" name="shop_id" value="{{ $shop_favorite['shop']['id'] }}">
-						@csrf
-						@method('delete')
-						<button class="button__submit-fav">&#10084;</button>
-					</form>
-					@endif
-					@endauth
+          @if($shop_favorite['favorite'] === 'true')
+          <form class="form__fav" action="/favorite/delete" method="post">
+            <input type="hidden" name="shop_id" value="{{ $shop_favorite['shop']['id'] }}">
+            @csrf
+            @method('delete')
+            <button class="button__submit-fav">&#10084;</button>
+          </form>
+          @endif
+          @endauth
 
-					@guest()
-					<a class="button__submit-un_fav" href="/login">&#10084;</a>
-					@endguest
+          @guest()
+          <a class="button__submit-un_fav" href="/login">&#10084;</a>
+          @endguest
 
-				</div>
-			</div>
-		</div>
-	</div>
-	@endforeach
+        </div>
+      </div>
+    </div>
+  </div>
+  @endforeach
 
 </div>
 @endsection
--- src/resources/views/review.blade.php
+++ src/resources/views/review.blade.php
@@ -6,104 +6,104 @@
 
 @section('content')
 <div class="content">
-	<a class="mypage__link" href="/mypage">&lt</a>
-	<h3 class="shop-visited__tittle">来店履歴</h3>
-	<div class="content__shop-visited">
-		@foreach($visited_shops as $visited_shop)
-		<div class="shop-visited__list">
+  <a class="mypage__link" href="/mypage">&lt</a>
+  <h3 class="shop-visited__tittle">来店履歴</h3>
+  <div class="content__shop-visited">
+    @foreach($visited_shops as $visited_shop)
+    <div class="shop-visited__list">
 
-			<div class="shop-visited__detail">
-				<table class="shop-visited__list-table">
-					<tr class="shop-visited__list-row">
-						<th class="shop-visited__list-data">Shop</th>
-						<td class="shop-visited__list-data">{{ $visited_shop->reservationShop->name }}</td>
-					</tr>
-					<th class="shop-visited__list-data">Date</th>
-					<td class="shop-visited__list-data">{{ $visited_shop->reservationDay() }}</td>
-					</tr>
-					<tr class="shop-visited__list-row">
-						<th class="shop-visited__list-data">Time</th>
-						<td class="shop-visited__list-data">{{ $visited_shop->reservationTime() }}</td>
-					</tr>
-					<tr class="shop-visited__list-row">
-						<th class="shop-visited__list-data">Number</th>
-						<td class="shop-visited__list-data">{{ $visited_shop['person_num'] }}人</td>
-					</tr>
-				</table>
-			</div>
-			<div class="review-list">
-				<!-- レビュー結果 -->
-				@if($visited_shop['stars'])
-				<table class="review__list-table">
-					<tr class="review-row">
-						<th class="review-data__th">stars</th>
-						<td class="review-data review-data__star" data-rate="{{ $visited_shop['stars'] }}"> <span class="review-data__data-rate">({{ $visited_shop['stars'] }})</span></td>
+      <div class="shop-visited__detail">
+        <table class="shop-visited__list-table">
+          <tr class="shop-visited__list-row">
+            <th class="shop-visited__list-data">Shop</th>
+            <td class="shop-visited__list-data">{{ $visited_shop->reservationShop->name }}</td>
+          </tr>
+          <th class="shop-visited__list-data">Date</th>
+          <td class="shop-visited__list-data">{{ $visited_shop->reservationDay() }}</td>
+          </tr>
+          <tr class="shop-visited__list-row">
+            <th class="shop-visited__list-data">Time</th>
+            <td class="shop-visited__list-data">{{ $visited_shop->reservationTime() }}</td>
+          </tr>
+          <tr class="shop-visited__list-row">
+            <th class="shop-visited__list-data">Number</th>
+            <td class="shop-visited__list-data">{{ $visited_shop['person_num'] }}人</td>
+          </tr>
+        </table>
+      </div>
+      <div class="review-list">
+        <!-- レビュー結果 -->
+        @if($visited_shop['stars'])
+        <table class="review__list-table">
+          <tr class="review-row">
+            <th class="review-data__th">stars</th>
+            <td class="review-data review-data__star" data-rate="{{ $visited_shop['stars'] }}"> <span class="review-data__data-rate">({{ $visited_shop['stars'] }})</span></td>
 
-					</tr>
-					<tr class=" review-row">
-						<th class="review-data">Nickname</th>
-						<td class="review-data">{{ $visited_shop['nickname'] }}</td>
-					</tr>
-					<tr class="review-row">
-						<th class="review-data" valign="top">Comment</th>
-						<td class="review-data review-data__text">{{ $visited_shop['comment'] }}</td>
-					</tr>
-				</table>
-				<p class="review-data__date">
-					{{ \Carbon\Carbon::parse($visited_shop['updated_at'])->format('Y-m-d') }}更新
-				</p>
+          </tr>
+          <tr class=" review-row">
+            <th class="review-data">Nickname</th>
+            <td class="review-data">{{ $visited_shop['nickname'] }}</td>
+          </tr>
+          <tr class="review-row">
+            <th class="review-data" valign="top">Comment</th>
+            <td class="review-data review-data__text">{{ $visited_shop['comment'] }}</td>
+          </tr>
+        </table>
+        <p class="review-data__date">
+          {{ \Carbon\Carbon::parse($visited_shop['updated_at'])->format('Y-m-d') }}更新
+        </p>
 
-				<!-- レビュー入力 -->
-				@else
-				<ul class="message">
-					<li class="message-list">{{ session('message') }}</li>
-					@foreach ($errors->all() as $error)
-					<li class="message-list">{{ $error }}</li>
-					@endforeach
-				</ul>
+        <!-- レビュー入力 -->
+        @else
+        <ul class="message">
+          <li class="message-list">{{ session('message') }}</li>
+          @foreach ($errors->all() as $error)
+          <li class="message-list">{{ $error }}</li>
+          @endforeach
+        </ul>
 
-				<table class="review__list-table--form">
-					<form class="review-form" action="/review/update" method="post">
-						<input type="hidden" name="id" value="{{ $visited_shop['id'] }}">
-						@csrf
-						@method('patch')
-						<tr class="review-row">
-							<th class="review-data">Stars</th>
-							<td class="review-data">
-								<div class="review-star">
-									@for($i = 5; $i >= 1; $i--)
-									<input class="review-star__input" id="star-{{ $i }}" type="radio" name="stars" value="{{ $i }}">
-									<label for="star-{{ $i }}">&#9733;</label>
-									@endfor
-								</div>
-							</td>
-						</tr>
-						<tr class="review-row">
-							<th class="review-data">Nickname</th>
-							<td class="review-data">
-								<input class="review-data__input" type="text" name="nickname" placeholder="未入力の場合「匿名」になります">
-							</td>
-						</tr>
-						<tr class="review-row">
-							<th class="review-data" valign="top">Comment</th>
-							<td class="review-data">
-								<textarea class="review-comment__input" name="comment" placeholder="レビュー内容を記入してください"></textarea>
-							</td>
-						</tr>
-				</table>
+        <table class="review__list-table--form">
+          <form class="review-form" action="/review/update" method="post">
+            <input type="hidden" name="id" value="{{ $visited_shop['id'] }}">
+            @csrf
+            @method('patch')
+            <tr class="review-row">
+              <th class="review-data">Stars</th>
+              <td class="review-data">
+                <div class="review-star">
+                  @for($i = 5; $i >= 1; $i--)
+                  <input class="review-star__input" id="star-{{ $i }}" type="radio" name="stars" value="{{ $i }}">
+                  <label for="star-{{ $i }}">&#9733;</label>
+                  @endfor
+                </div>
+              </td>
+            </tr>
+            <tr class="review-row">
+              <th class="review-data">Nickname</th>
+              <td class="review-data">
+                <input class="review-data__input" type="text" name="nickname" placeholder="未入力の場合「匿名」になります">
+              </td>
+            </tr>
+            <tr class="review-row">
+              <th class="review-data" valign="top">Comment</th>
+              <td class="review-data">
+                <textarea class="review-comment__input" name="comment" placeholder="レビュー内容を記入してください"></textarea>
+              </td>
+            </tr>
+        </table>
 
-				<div class="review__button">
-					<button class="review__button-submit">送信</button>
-				</div>
-				</form>
-				@endif
-			</div>
+        <div class="review__button">
+          <button class="review__button-submit">送信</button>
+        </div>
+        </form>
+        @endif
+      </div>
 
-		</div>
-		@endforeach
+    </div>
+    @endforeach
 
-	</div>
-	{{ $visited_shops->links('vendor.pagination.bootstrap-4') }}
+  </div>
+  {{ $visited_shops->links('vendor.pagination.bootstrap-4') }}
 </div>
 
 
